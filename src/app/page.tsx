cb6329c78f5890769cb76c7892f148bf88cc27db
ahmadmidlaj313@gmail.com <you@example.com>
2024-05-03 10:39:06 +0530

    prod build bugs fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "./_components/Hero";
 import Herofirst from "./_components/Herofirst";
 import HowitWorks from "./_components/HowitWorks";
 import Navbar from "./_components/Navbar";
-import Team from "./_components/Team";
+// import Team from "./_components/Team";
 import Testimonials from "./_components/Testimonials";
 import Aurora from "./_components/Aurora";
 import Contact from "./_components/Contact";
