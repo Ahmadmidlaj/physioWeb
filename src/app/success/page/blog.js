d1348534717e2b8bf43e602a24ffe5f7abe539a7
afrad984 <123197654+afrad984@users.noreply.github.com>
2024-10-17 23:55:38 +0530

    Update blog.js

--- src/app/success/page/blog.js
+++ src/app/success/page/blog.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 
 const posts = [
     { id: 1, title: 'First Post', content: 'This is the content of my first post.' },
-    { id: 2, title: 'Second Post', content: 'This is the content of my second post.' },
 ];
 
 export default function Blog() {
