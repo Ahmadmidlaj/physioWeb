53182a4d1269bbc2d5de36a54824deb01fba0f1f
afrad984 <123197654+afrad984@users.noreply.github.com>
2024-10-17 23:54:29 +0530

    Update blog.js

--- src/app/success/page/blog.js
+++ src/app/success/page/blog.js
@@ -1,1 +1,24 @@
+import Link from 'next/link';
+
+const posts = [
+    { id: 1, title: 'First Post', content: 'This is the content of my first post.' },
+    { id: 2, title: 'Second Post', content: 'This is the content of my second post.' },
+];
+
+export default function Blog() {
+    return (
+        <div>
+            <h1>My Blog</h1>
+            {posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.content}</p>
+                    <Link href={`/blog/${post.id}`}>
+                        <a>Read more...</a>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
+}
 
