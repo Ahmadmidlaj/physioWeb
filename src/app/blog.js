f2a10ab08629cbf0c97d3fea33726d097d05a137
afrad984 <123197654+afrad984@users.noreply.github.com>
2024-10-17 23:33:00 +0530

    Update blog.js

--- src/app/blog.js
+++ src/app/blog.js
@@ -1,22 +1,12 @@
 import Link from 'next/link';
 
-const posts = [
-    { id: 1, title: 'First Post', content: 'This is the content of my first post.' },
-    { id: 2, title: 'Second Post', content: 'This is the content of my second post.' },
-];
-
-export default function Blog() {
+export default function Home() {
     return (
         <div>
-            <h1>My Blog</h1>
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.content}</p>
-                    <Link href={`/blog/${post.id}`}>
-                        <a>Read more...</a>
-                    </Link>
-                </div>
-            ))}
+            <h1>Welcome to My Blog</h1>
+            <Link href="/blog">
+                <a>Go to Blog</a>
+            </Link>
         </div>
     );
 }
