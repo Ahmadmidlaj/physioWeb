6b7da2bcc7bdb913b2ad713fbe654d45102c3204
afrad984 <123197654+afrad984@users.noreply.github.com>
2024-10-18 11:29:10 +0530

    Update blog.tsx

--- src/app/_components/blog.tsx
+++ src/app/_components/blog.tsx
@@ -1,1 +1,21 @@
+// pages/about.js
+
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>About Us</title>
+        <meta name="description" content="Learn more about us." />
+      </Head>
+      <div>
+        <h1>About Us</h1>
+        <p>Welcome to our about page! We are dedicated to providing the best service.</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
 
