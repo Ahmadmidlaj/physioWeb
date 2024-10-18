554984cbd249c312f53bfea6273c47d95c8c1a7e
afrad984 <123197654+afrad984@users.noreply.github.com>
2024-10-18 11:34:07 +0530

    Update blog.tsx

--- src/app/_components/blog.tsx
+++ src/app/_components/blog.tsx
@@ -1,16 +1,16 @@
  <header className="bg-white sticky top-0 z-10">
     <>
       <Head>
-        <title>About Us</title>
+        <titleblog</title>
         <meta name="description" content="Learn more about us." />
       </Head>
       <div>
-        <h1>About Us</h1>
+        <h1>blog</h1>
         <p>Welcome to our about page! We are dedicated to providing the best service.</p>
       </div>
     </>
   );
 };
 
-export default About;
+export default blog;
 
